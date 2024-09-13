bc7b2569d0bf3dd76585a7f4f5580230916f49c8
Leonardo Minora <leonardo.minora@gmail.com>
2024-09-13 20:22:59 -0300

    adicionado endpoint para receber 1 arquivo sem validações

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,7 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import {
+  ApiBody,
+  ApiConsumes,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { UploadService } from './upload.service';
 
 @Controller('upload')
+@ApiTags('upload')
 export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
+
+  @Post('exemplo-simples')
+  @UseInterceptors(FileInterceptor('arquivo'))
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        arquivo: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  @ApiOperation({ summary: 'Exemplo de upload de 1 arquivo qualquer' })
+  @ApiResponse({ status: 201, description: 'Arquivo enviado com sucesso.' })
+  @ApiResponse({ status: 400, description: 'Erro no envio do arquivo.' })
+  uploadArquivoSimples(@UploadedFile() arq: Express.Multer.File) {
+    console.log(arq);
+
+    return {
+      estado: 'ok',
+    };
+  }
 }
