bd423caa0fca6d2d4f1b054edc00a5b7ad95eeec
Leonardo Minora <leonardo.minora@gmail.com>
2024-09-17 19:38:16 -0300

    implementado o endpoint para salvar arquivo em disco local

--- src/armazenamento/armazenamento.controller.ts
+++ src/armazenamento/armazenamento.controller.ts
@@ -1,8 +1,42 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import {
+  ApiBody,
+  ApiConsumes,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { ArmazenamentoService } from './armazenamento.service';
 
 @Controller('armazenamento')
+@ApiTags('armazenamento')
 export class ArmazenamentoController {
   constructor(private readonly armazenamentoService: ArmazenamentoService) {}
+
+  @Post()
+  @UseInterceptors(FileInterceptor('imagem'))
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        imagem: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  @ApiOperation({ summary: 'Upload de arquivo com armazenamento' })
+  @ApiResponse({ status: 201, description: 'Arquivo enviado com sucesso.' })
+  @ApiResponse({ status: 400, description: 'Erro no envio do arquivo.' })
+  salvar(@UploadedFile() arq: Express.Multer.File) {
+    return this.armazenamentoService.salvarEmDisco(arq);
+  }
 }
--- src/armazenamento/armazenamento.service.ts
+++ src/armazenamento/armazenamento.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { promises as fs } from 'fs';
+import * as path from 'path';
+
+class ImagemDto {
+  id: string;
+  nome: string;
+  tamanho: number;
+  mimetype: string;
+  encoding: string;
+  armazenamento: string;
+}
 
 @Injectable()
-export class ArmazenamentoService {}
+export class ArmazenamentoService {
+  dir = path.join(__dirname, '..', '..', 'uploads');
+  imagens = new Array<ImagemDto>();
+
+  async salvarEmDisco(arquivo: Express.Multer.File) {
+    const nomeCompleto = path.join(this.dir, arquivo.originalname);
+
+    await fs.writeFile(nomeCompleto, arquivo.buffer);
+    this.imagens.push({
+      id: arquivo.originalname,
+      nome: arquivo.originalname,
+      tamanho: arquivo.size,
+      mimetype: arquivo.mimetype,
+      encoding: arquivo.encoding,
+      armazenamento: nomeCompleto,
+    });
+    return {
+      estado: 'ok',
+      dados: {
+        id: arquivo.originalname,
+        nome: arquivo.originalname,
+      },
+    };
+  }
+}
