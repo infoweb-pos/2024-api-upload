ce6634b93c80b8777a53ba7c8342f9fc8a6875f1
Leonardo Minora <leonardo.minora@gmail.com>
2024-09-19 16:28:33 -0300

    adicionado endpoint para armazenar arquivo nas nuvens

--- src/nuvem/nuvem.controller.ts
+++ src/nuvem/nuvem.controller.ts
@@ -1,7 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { NuvemService } from './nuvem.service';
+import {
+  ApiBody,
+  ApiConsumes,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('nuvem')
+@ApiTags('nuvem')
 export class NuvemController {
   constructor(private readonly nuvemService: NuvemService) {}
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('arquivo'))
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        arquivo: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  @ApiOperation({ summary: 'Upload de arquivo com armazenamento na nuvem' })
+  @ApiResponse({ status: 201, description: 'Upload de arquivo concluído.' })
+  @ApiResponse({ status: 400, description: 'ERRO no upload do arquivo.' })
+  upload(@UploadedFile() arquivo: Express.Multer.File) {
+    return this.nuvemService.upload(arquivo);
+  }
 }
--- src/nuvem/nuvem.service.ts
+++ src/nuvem/nuvem.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { ImageKitService } from 'imagekit-nestjs';
+
+class ArquivoDto {
+  id: string;
+  nome: string;
+  tamanho: number;
+  mimetype: string;
+  encoding: string;
+  armazenamento: string;
+}
 
 @Injectable()
-export class NuvemService {}
+export class NuvemService {
+  arquivos: ArquivoDto[] = [];
+
+  constructor(private readonly cloud: ImageKitService) {}
+
+  async upload(arquivo: Express.Multer.File) {
+    const resultado = await this.cloud.upload({
+      file: arquivo.buffer,
+      fileName: arquivo.originalname,
+    });
+
+    if (!resultado) {
+      throw new HttpException(
+        'Erro ao tentar armazenar arquivo na nuvem',
+        HttpStatus.NOT_ACCEPTABLE,
+      );
+    }
+
+    const informacao: ArquivoDto = {
+      id: resultado.fileId,
+      nome: arquivo.originalname,
+      encoding: arquivo.encoding,
+      mimetype: arquivo.mimetype,
+      tamanho: arquivo.size,
+      armazenamento: resultado.url,
+    };
+    this.arquivos.push(informacao);
+    return {
+      estado: 'ok',
+      data: informacao,
+    };
+  }
+}
